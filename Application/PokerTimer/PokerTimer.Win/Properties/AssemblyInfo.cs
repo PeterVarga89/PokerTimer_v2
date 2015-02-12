3e5efbae926f01b77fa7c5e6a191fa29a4da5002
Peter Varga <petervarga@rerecrew.com>
2015-02-12 01:13:51 +0100

    added folders, added DLLs

--- Application/PokerTimer/PokerTimer.Win/Properties/AssemblyInfo.cs
+++ Application/PokerTimer/PokerTimer.Win/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PokerTimer.Win")]
+[assembly: AssemblyTitle("PokerTimer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PokerTimer.Win")]
+[assembly: AssemblyCompany("Club Arcada")]
+[assembly: AssemblyProduct("PokerTimer")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
